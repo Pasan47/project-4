71044f6d20bbb557d938bdb7cd53c7339f095337
monicasuri <bi2mon@gmail.com>
2025-06-26 10:49:24 +0530

    Todo App first commit

---
+++ test/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if(task.trim()) {
+      setTodos([...todos, {Text: task, completed: false}])
+      setTask('')
+    }
+  }
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
+  return (
+    <>
+      <div className="app">
+        <h1>Todo List</h1>
+        <div className="input-section">
+          <input type="text" 
+          value={task} 
+          onChange={(e) => setTask(e.target.value)}
+          placeholder='Add a new task' />
+
+          <button onClick={addTodo}>Submit</button>
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index} className={todo.completed ? 'completed' : '' }>
+              <span onClick={() => toggleTodo(index)} > {todo.Text} </span>
+              <button onClick={() => deleteTodo(index) } >Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
